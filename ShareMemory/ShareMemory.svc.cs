2f5f93c34e94423c3ab2d1f7e30e721fd5b3ab6b
ziizfn <515868058>
2014-06-22 17:59:28 +0800

    change  the name for sharememory and add unit test

--- ShareMemory/ShareMemory.svc.cs
+++ ShareMemory/ShareMemory.svc.cs
@@ -14,7 +14,7 @@ namespace ShareMemory
 {
     // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码、svc 和配置文件中的类名“Service1”。
     // 注意: 为了启动 WCF 测试客户端以测试此服务，请在解决方案资源管理器中选择 Service1.svc 或 Service1.svc.cs，然后开始调试。
-    public class Service1 : IService1
+    public class ShareMemory : IShareMemory
     {
 
         public string GetData(GetdataRequest getdataRequest)
@@ -24,7 +24,7 @@ namespace ShareMemory
 
 
 
-        bool IService1.SetData(SetdataRequest setdataRequest)
+        bool IShareMemory.SetData(SetdataRequest setdataRequest)
         {
             return OperationWork.Setdata(setdataRequest);
         }
